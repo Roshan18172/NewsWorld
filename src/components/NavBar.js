f62589b59604fa26e3ea7a5d3b769236bafce014
Roshan Pal <roshanpal18172@gmail.com>
2025-08-10 21:08:45 +0530

    Add routing and category navigation to news app
    Integrated React Router for navigation between news categories. Updated NavBar to use Link components for category navigation. Refactored News component to accept country and category as props, enabling dynamic API requests based on route.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 // import PropTypes from 'prop-types' 
 
 export class NavBar extends Component {
@@ -9,42 +9,28 @@ export class NavBar extends Component {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
             <div className="container-fluid">
-                <a className="navbar-brand" href="/">NewsWorld</a>
+                <Link className="navbar-brand" to="/">NewsWorld</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="/">Home</a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link active" href="/about">About</a>
-                    </li>
-                    <li className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown
-                    </a>
-                    <ul className="dropdown-menu">
-                        <li><a className="dropdown-item" href="/">Action</a></li>
-                        <li><a className="dropdown-item" href="/">Another action</a></li>
-                        <li><hr className="dropdown-divider"/></li>
-                        <li><a className="dropdown-item" href="/">Something else here</a></li>
-                    </ul>
-                    </li>
-                    <li className="nav-item">
-                    <button className="nav-link disabled" type="button" aria-disabled="true" tabIndex="-1">Disabled</button>
-                    </li>
+                    <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                    </li>                    
+                    <li className="nav-item"><Link className="nav-link active" to="/business">Business</Link></li>
+                    <li className="nav-item"><Link className="nav-link active" to="/entertainment">Entertainment</Link></li>
+                    <li className="nav-item"><Link className="nav-link active" to="/general">General</Link></li>
+                    <li className="nav-item"><Link className="nav-link active" to="/health">Health</Link></li>
+                    <li className="nav-item"><Link className="nav-link active" to="/science">Science</Link></li>
+                    <li className="nav-item"><Link className="nav-link active" to="/sports">Sports</Link></li>
+                    <li className="nav-item"><Link className="nav-link active" to="/technology">Technology</Link></li>
                 </ul>
-                <form className="d-flex" role="search">
-                    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                    <button className="btn btn-success" type="submit">Search</button>
-                </form>
                 </div>
             </div>
-            <a className="navbar-brand" href="/">
+            <Link className="navbar-brand" to="/">
                 <img src="news.png" alt="Logo" width="35" height="35" className="d-inline-block align-text-top" />
-             </a>
+             </Link>
         </nav>
     )
   }
--- src/components/News.js
+++ src/components/News.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
-import Spinner from './Spinner';
+import Spinner from './Spinner'
+import PropTypes from 'prop-types'
 
 export class News extends Component {
+    static defaultProps = {
+        country: 'us',
+        pageSize: 4,
+        category: 'general'
+    }
+    static propTypes = {
+        country: PropTypes.string,
+        pageSize: PropTypes.number,
+        category: PropTypes.string
+    }
 
     async componentDidMount() {
         // Simulating an API call to fetch news articles
         // In a real application, you would replace this with an actual API call
-        let url= `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=46efa12190ad4529ac392f8a69578a01&page=1&pageSize=${this.props.pageSize}`;
+        let url= `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=46efa12190ad4529ac392f8a69578a01&page=1&pageSize=${this.props.pageSize}`;
         this.setState({ loading: true });
         let data = await fetch(url);
         let parsedData = await data.json();
@@ -30,7 +41,7 @@ export class News extends Component {
     }
     handlePrevClick = async () => {
         console.log("Previous");
-        let url= `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=46efa12190ad4529ac392f8a69578a01&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+        let url= `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=46efa12190ad4529ac392f8a69578a01&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
         this.setState({ loading: true });
         let data = await fetch(url);
         let parsedData = await data.json();
@@ -43,7 +54,7 @@ export class News extends Component {
     handleNextClick = async () => {
         console.log("Next");
         if (!(this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize))) {
-            let url= `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=46efa12190ad4529ac392f8a69578a01&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+            let url= `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=46efa12190ad4529ac392f8a69578a01&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
             this.setState({ loading: true });
             let data = await fetch(url);
             let parsedData = await data.json();
