e82f830508a16bbf34d85dded3d37cbc2a4f64de
Roshan Pal <roshanpal18172@gmail.com>
2025-08-11 12:23:25 +0530

    add loading bar and hiding api key
    by importing loading bar after installation of the package and creating a local environment file to hide the api key

--- src/App.js
+++ src/App.js
@@ -6,22 +6,35 @@ import NavBar from './components/NavBar';
 import News from './components/News';
 import Footer from './components/Footer';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoadingBar from "react-top-loading-bar";
 
 export default class App extends Component {
+  apiKey = process.env.REACT_APP_NEWS_API_KEY ; // Fallback API key if not set in .env.local
+  state = {
+    progress: 0
+  }
+  setProgress = (progress) => {
+    this.setState({ progress: progress });
+  }
   render() {
     return (
       <div>
         <Router>
           <NavBar />
+          <LoadingBar
+            height={4}
+            color="#f11946"
+            progress={this.state.progress}
+          />
           <Routes>
-            <Route exact path="/" element={<News key="general" pageSize={4} country="us" category="general" />} />
-            <Route exact path="/business" element={<News key="business" pageSize={4} country="us" category="business" />} />
-            <Route exact path="/entertainment" element={<News key="entertainment" pageSize={4} country="us" category="entertainment" />} />
-            <Route exact path="/general" element={<News key="general" pageSize={4} country="us" category="general" />} />
-            <Route exact path="/health" element={<News key="health" pageSize={4} country="us" category="health" />} />
-            <Route exact path="/science" element={<News key="science" pageSize={4} country="us" category="science" />} />
-            <Route exact path="/sports" element={<News key="sports" pageSize={4} country="us" category="sports" />} />
-            <Route exact path="/technology" element={<News key="technology" pageSize={4} country="us" category="technology" />} />
+            <Route exact path="/" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="general" pageSize={4} country="us" category="general" />} />
+            <Route exact path="/business" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="business" pageSize={4} country="us" category="business" />} />
+            <Route exact path="/entertainment" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="entertainment" pageSize={4} country="us" category="entertainment" />} />
+            <Route exact path="/general" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="general" pageSize={4} country="us" category="general" />} />
+            <Route exact path="/health" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="health" pageSize={4} country="us" category="health" />} />
+            <Route exact path="/science" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="science" pageSize={4} country="us" category="science" />} />
+            <Route exact path="/sports" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="sports" pageSize={4} country="us" category="sports" />} />
+            <Route exact path="/technology" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="technology" pageSize={4} country="us" category="technology" />} />
           </Routes>
         </Router>
         <Footer />
--- src/components/News.js
+++ src/components/News.js
@@ -3,6 +3,7 @@ import NewsItem from './NewsItem'
 import Spinner from './Spinner'
 import PropTypes from 'prop-types'
 
+
 export class News extends Component {
     static defaultProps = {
         country: 'us',
@@ -29,17 +30,23 @@ export class News extends Component {
         }
     }
     async updateOnRequest() {
-        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=a9185dcd058c4b849a9fba190478b879&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+        this.props.setProgress(10);
+        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
         this.setState({ loading: true });
+        this.props.setProgress(30);
         let data = await fetch(url);
+        this.props.setProgress(50);
         let parsedData = await data.json();
+        this.props.setProgress(70);
         this.setState({
             articles: parsedData.articles,
             loading: false,
             totalResults: parsedData.totalResults,
             page: this.state.page
         });
+        this.props.setProgress(90);
         console.log(parsedData);
+        this.props.setProgress(100);
     }
     handlePrevClick = async () => {
         this.setState({ page: this.state.page - 1 });
@@ -57,11 +64,11 @@ export class News extends Component {
     render() {
         return (
             <div className="container-fluid ">
-                <h2 className="my-3 text-center">News World - Top Headlines</h2>
+                <h2 className="my-3 text-center">News World - Top {this.props.category.charAt(0).toUpperCase() + this.props.category.slice(1)} Headlines</h2>
                 {this.state.loading && <Spinner />}
                 <div className="container m-auto row bg-warning " style={{ marginBottom: "160px" }}>
                     {!this.state.loading && this.state.articles.map((element) => {
-                        return <div className="col-md-3 my-4" key={element.title}>
+                        return <div className="col-12 col-sm-6 col-md-4 col-lg-3 my-4" key={element.title}>
                             <NewsItem title={element.title ? element.title.slice(0, 52) : ""}
                                 description={element.description ? element.description.slice(0, 80) : ""}
                                 imageUrl={element.urlToImage ? element.urlToImage : "https://media.istockphoto.com/id/1409329028/vector/no-picture-available-placeholder-thumbnail-icon-illustration-design.jpg?s=612x612&w=0&k=20&c=_zOuJu755g2eEUioiOUdz_mHKJQJn-tDgIAhQzyeKUQ="}
