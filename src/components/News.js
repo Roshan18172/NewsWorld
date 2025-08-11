5eda2974185ed51ca03b18129558678df529b209
Roshan Pal <roshanpal18172@gmail.com>
2025-08-11 10:49:12 +0530

    Refactor news fetching and pagination logic
    Moved API request logic to a new updateOnRequest method and updated pagination handlers to use it. Set document title on mount and improved page state management for next/previous navigation. Also updated API key in the request URL.

--- src/components/News.js
+++ src/components/News.js
@@ -16,19 +16,8 @@ export class News extends Component {
     }
 
     async componentDidMount() {
-        // Simulating an API call to fetch news articles
-        // In a real application, you would replace this with an actual API call
-        let url= `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=46efa12190ad4529ac392f8a69578a01&page=1&pageSize=${this.props.pageSize}`;
-        this.setState({ loading: true });
-        let data = await fetch(url);
-        let parsedData = await data.json();
-        console.log(parsedData);
-        this.setState({
-            articles: parsedData.articles,
-            loading: false,
-            totalResults: parsedData.totalResults,
-            page: 1
-        });
+        document.title = `${this.props.category.charAt(0).toUpperCase() + this.props.category.slice(1)} - News World`;
+        this.updateOnRequest();
     }
     constructor() {
         super();
@@ -39,57 +28,59 @@ export class News extends Component {
             page: 1
         }
     }
-    handlePrevClick = async () => {
-        let url= `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=46efa12190ad4529ac392f8a69578a01&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+    async updateOnRequest() {
+        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=a9185dcd058c4b849a9fba190478b879&page=${this.state.page}&pageSize=${this.props.pageSize}`;
         this.setState({ loading: true });
         let data = await fetch(url);
         let parsedData = await data.json();
         this.setState({
-            page: this.state.page - 1,
             articles: parsedData.articles,
-            loading: false
+            loading: false,
+            totalResults: parsedData.totalResults,
+            page: this.state.page
         });
+        console.log(parsedData);
+    }
+    handlePrevClick = async () => {
+        this.setState({ page: this.state.page - 1 });
+        this.updateOnRequest();
     }
     handleNextClick = async () => {
-        if (!(this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize))) {
-            let url= `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=46efa12190ad4529ac392f8a69578a01&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
-            this.setState({ loading: true });
-            let data = await fetch(url);
-            let parsedData = await data.json();
-            this.setState({
-                page: this.state.page + 1,
-                articles: parsedData.articles,
-                loading: false
-            });
+        const maxPage = Math.ceil(this.state.totalResults / this.props.pageSize);
+        if (this.state.page < maxPage) {
+            this.setState(
+                prevState => ({ page: prevState.page + 1 }),
+                () => this.updateOnRequest()
+            );
         }
     }
-  render() {
-    return (
-      <div className="container-fluid ">
-        <h2 className="my-3 text-center">News World - Top Headlines</h2>
-        {this.state.loading && <Spinner />}
-        <div className="container m-auto row bg-warning " style={{ marginBottom: "160px" }}>
-            {!this.state.loading && this.state.articles.map((element) => {
-                return <div className="col-md-3 my-4" key={element.title}>
-                    <NewsItem title={element.title ? element.title.slice(0, 52) : ""} 
-                            description={element.description ? element.description.slice(0, 80) : ""} 
-                            imageUrl={element.urlToImage ? element.urlToImage : "https://media.istockphoto.com/id/1409329028/vector/no-picture-available-placeholder-thumbnail-icon-illustration-design.jpg?s=612x612&w=0&k=20&c=_zOuJu755g2eEUioiOUdz_mHKJQJn-tDgIAhQzyeKUQ="} 
-                            newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source} />
-                    </div>
-            })}
+    render() {
+        return (
+            <div className="container-fluid ">
+                <h2 className="my-3 text-center">News World - Top Headlines</h2>
+                {this.state.loading && <Spinner />}
+                <div className="container m-auto row bg-warning " style={{ marginBottom: "160px" }}>
+                    {!this.state.loading && this.state.articles.map((element) => {
+                        return <div className="col-md-3 my-4" key={element.title}>
+                            <NewsItem title={element.title ? element.title.slice(0, 52) : ""}
+                                description={element.description ? element.description.slice(0, 80) : ""}
+                                imageUrl={element.urlToImage ? element.urlToImage : "https://media.istockphoto.com/id/1409329028/vector/no-picture-available-placeholder-thumbnail-icon-illustration-design.jpg?s=612x612&w=0&k=20&c=_zOuJu755g2eEUioiOUdz_mHKJQJn-tDgIAhQzyeKUQ="}
+                                newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source} />
+                        </div>
+                    })}
 
-        </div>
-        <div className="container d-flex justify-content-between my-3" style={{ height: "80px" }}>
+                </div>
+                <div className="container d-flex justify-content-between my-3" style={{ height: "80px" }}>
 
-        </div>
-        <div className="container d-flex justify-content-between my-3 fixed-bottom bg-success ">
-            <button disabled={this.state.page<=1} type="button" className="btn btn-dark my-3" onClick={this.handlePrevClick}> &larr; Previous</button>
-            <p className="text-center m-auto">Page {this.state.page} of {Math.ceil(this.state.totalResults / this.props.pageSize)}</p>
-            <button  type="button" className="btn btn-dark my-3" onClick={this.handleNextClick}>Next &rarr;</button>
-        </div>
-      </div>
-    )
-  }
+                </div>
+                <div className="container d-flex justify-content-between my-3 fixed-bottom bg-success ">
+                    <button disabled={this.state.page <= 1} type="button" className="btn btn-dark my-3" onClick={this.handlePrevClick}> &larr; Previous</button>
+                    <p className="text-center m-auto">Page {this.state.page} of {Math.ceil(this.state.totalResults / this.props.pageSize)}</p>
+                    <button type="button" className="btn btn-dark my-3" onClick={this.handleNextClick}>Next &rarr;</button>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default News
